40234e152ace00dcd05ed56b96b3eff7ca23151d
Brice Errandonea <berrandonea@yahoo.fr>
2016-10-25 11:30:59 +0200

    Create local_problemsection.php

---
+++ lang/en/local_problemsection.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Initially developped for :
+ * Université de Cergy-Pontoise
+ * 33, boulevard du Port
+ * 95011 Cergy-Pontoise cedex
+ * FRANCE
+ *
+ * Adds to the course a section where the teacher can submit a problem to groups of students 
+ * and give them various collaboration tools to work together on a solution. 
+ *
+ * @package   local_problemsection
+ * @copyright 2016 Brice Errandonea <brice.errandonea@u-cergy.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * 
+ * File : lang/en/local_problemsection.php
+ * English text strings.
+ */
+
+$string['pluginname'] = 'Problem section';
+$string['problemsection:addinstance'] = 'Add a new problem section';
+$string['communicationtools'] = 'Communication tools';
+$string['copygrouping'] = 'Copy an existing grouping';
+$string['nbgroups'] = 'Groups number';
+$string['groupsworking'] = 'Groups working on';
+$string['for'] = 'for';
+$string['pleaseuse'] = 'Please use this tool to work with your group on';
+$string['clearallgroups'] = 'Clear all groups';
+$string['cleargroup'] = 'Clear group';
+$string['creategroup'] = 'Create another group';
+$string['manage'] = 'Problem sections';
+$string['managegroups'] = 'Manage groups';
+$string['sharedgroup'] = 'This group is also used by';
+$string['sharedchanges'] = 'Any change here will be sent back there.';
+$string['deleteproblemsection'] = 'Delete the problem section';
+$string['warningdelete'] = 'This will delete the problem section "{$a}" and all its content but the groups. Are you sure ?';
+$string['noproblemyet'] = 'There\'s not any problem section yet in this course.';
+$string['summary'] = 'Summary';
+$string['summary_help'] = 'A brief text presenting the problem section. It will be displayed on the course page, just after the section title.';
+$string['directions'] = 'Directions';
+$string['directions_help'] = 'Explain your students what they have to do. This will be displayed inside the Assign activity.';
