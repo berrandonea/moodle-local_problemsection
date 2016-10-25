3590e834288a0d165051f0f76fd2e7e6f55b9fa8
Brice Errandonea <berrandonea@yahoo.fr>
2016-10-25 11:31:29 +0200

    Create local_problemsection.php

---
+++ lang/fr/local_problemsection.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Initially developped for :
+ * Université de Cergy-Pontoise
+ * 33, boulevard du Port
+ * 95011 Cergy-Pontoise cedex
+ * FRANCE
+ *
+ * Adds to the course a section where the teacher can submit a problem to groups of students 
+ * and give them various collaboration tools to work together on a solution. 
+ *
+ * @package   local_problemsection
+ * @copyright 2016 Brice Errandonea <brice.errandonea@u-cergy.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * 
+ * File : lang/fr/local_problemsection.php
+ * French text strings.
+ */
+
+$string['pluginname'] = 'Section problème';
+$string['problemsection:addinstance'] = 'Ajouter une nouvelle section problème';
+$string['theproblem'] = 'Voici la situation';
+$string['theproblem_help'] = 'Expliquez à vos étudiants le problème à résoudre.';
+$string['expectation'] = 'Travail attendu';
+$string['expectation_help'] = 'Expliquez à vos étudiants quel type de travail vous attendez d\'eux et sous quelle forme ils doivent le rendre.';
+$string['communicationtools'] = 'Outils de communication';
+$string['copygrouping'] = 'Copier un groupement existant';
+$string['nbgroups'] = 'Nombre de groupes';
+$string['groupsworking'] = 'Groupe travaillant sur';
+$string['for'] = 'pour';
+$string['pleaseuse'] = 'Merci d\'utiliser cet outil pour travailler avec votre groupe sur';
+$string['clearallgroups'] = 'Vider tous les groupes';
+$string['cleargroup'] = 'Vider le groupe';
+$string['creategroup'] = 'Créer un autre groupe';
+$string['manage'] = 'Sections problèmes';
+$string['managegroups'] = 'Gérer les groupes';
+$string['sharedgroup'] = 'Ce groupe travaille aussi sur';
+$string['sharedchanges'] = 'Vos changements ici seront répercutés là-bas.';
+$string['deleteproblemsection'] = 'Supprimer la section problème';
+$string['warningdelete'] = 'Cette action supprimera la section problème "{$a}" et tout son contenu, à l\'exception des groupes. Confirmez-vous ?';
+$string['noproblemyet'] = 'Il n\'y a pas encore de section problème dans ce cours.';
+$string['summary'] = 'Résumé';
+$string['summary_help'] = 'Une courte présentation du problème. Elle sera affichée sur la page du cours, juste en dessous du titre de la section.';
+$string['directions'] = 'Consignes';
+$string['directions_help'] = 'Expliquez à vos étudiants ce qu\'ils doivent faire. Cela s\'affichera à l\'intérieur de l\'activité Devoir.';
